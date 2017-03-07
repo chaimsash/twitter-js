e94d95306671c62e33f9b54961151a553285c30f
zc <zachcaceres@gmail.com>
2017-03-07 11:06:28 -0500

    refactor app.js

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
-const express = require( 'express' );
-const nunjucks = require( 'nunjucks' );
-
+const express = require('express');
+const nunjucks = require('nunjucks');
 const app = express(); // creates an instance of an express application
 
-app.listen(3000, function(){
-  console.log('server listening');
+// Nunjucks configure
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app,
+    noCache: true
 });
 
-nunjucks.configure('views',{
-  autoescape: true,
-  express: app,
-  noCache: true
+
+// WTF?
+// TYING FILE EXTENSION TO THE RENDERING ENGINE
+app.set('view engine', 'html');
+// app.engine(
+//     'html', nunjucks.render
+// );
+
+app.listen(3000, function() {
+    console.log('server listening');
 });
+
+function Person(name) {
+    this.name = name;
+}
+var people = [new Person('Dopey'), new Person('Sleepy'), new Person('Drowsey')]
+
+
+var indexData = {
+    people: people,
+    title: 'The 7 twitter dwarves'
+}
+
 //logging middlewear extra credit
 // function logger(){
 //   [].prototype.slice.call(arguments).forEach(function(x){
@@ -19,16 +39,8 @@ nunjucks.configure('views',{
 //   });
 // }
 
-function Person(name){
-  this.name = name;
-}
 
-var people = [new Person('Dopey'), new Person('Sleepy'), new Person('Drowsey')]
-
-var indexData = {people: people, title: 'The 7 twitter dwarves'}
-
-var index = nunjucks.render('index.html', indexData);
-
-app.get('/**', function(request, response){
-  response.send(index);
+app.get('/**', function(request, response) {
+    // Renders and SENDS the Html
+    response.render('index', indexData);
 });
